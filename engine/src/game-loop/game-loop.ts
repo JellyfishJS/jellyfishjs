74aef72b6be94fda3a34b4edf2a56683c777496f
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2019-09-12 17:19:57 -0400

    Add the game loop

---
+++ engine/src/game-loop/game-loop.ts
@@ -0,0 +1,62 @@
+import { GameObject } from "../game-object/game-object";
+
+/**
+ * How many times to add the objects that have been created
+ * in one game loop.
+ *
+ * GameObjects may create other GameObjects in their onCreate,
+ * so the creation needs to be handled multiple times per loop.
+ * However, if this is recursive it will loop forever,
+ * so there needs to be some sort of limit.
+ */
+const maxCreationDepth = 1000;
+
+/**
+ * Handles running the game loop over the GameObjects it owns.
+ */
+export class GameLoop {
+
+    /**
+     * An array of the GameObjects in this GameLoop
+     * that will be created on the next step.
+     */
+    private _gameObjectsToBeCreated: GameObject[] = [];
+
+    /**
+     * Adds all the game objects that have been created the previous loop
+     * to the game objects to be handled this loop.
+     */
+    private _handleCreation() {
+        let iterations = 0;
+        while (this._gameObjectsToBeCreated.length !== 0 && ++iterations < maxCreationDepth) {
+            this._gameObjects = [...this._gameObjects, ...this._gameObjectsToBeCreated];
+            const gameObjectsCreatedThisIteration = this._gameObjectsToBeCreated;
+            this._gameObjectsToBeCreated = [];
+            gameObjectsCreatedThisIteration.forEach((gameObject) => gameObject["_onCreateHandler"].callCallback());
+        }
+        if (iterations === maxCreationDepth) {
+            console.error("Recursive object creation detected. Some object is probably creating itself in its `create`.");
+        }
+    }
+
+    /**
+     * An array of the GameObjects that exist in this game loop.
+     */
+    private _gameObjects: GameObject[] = [];
+
+    /**
+     * Adds the specified GameObject to this GameLoop.
+     */
+    public addGameObject(object: GameObject) {
+        this._gameObjectsToBeCreated.push(object);
+    }
+
+    /**
+     * Runs a single game loop.
+     */
+    public runLoop() {
+        this._handleCreation();
+        this._gameObjects.forEach((gameObject) => gameObject["_onStepHandler"].callCallback());
+    }
+
+}
