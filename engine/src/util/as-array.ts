928e1dbb0abc37bc220d7b1add43d331a8bf47df
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2019-09-26 17:34:56 -0400

    Add export

--- engine/src/util/as-array.ts
+++ engine/src/util/as-array.ts
@@ -2,7 +2,7 @@
  * Takes undefined, a thing, or an array of things,
  * and returns an array containing any things passed to it.
  */
-function asArray<T>(value: undefined | T | T[]): T[] {
+export function asArray<T>(value: undefined | T | T[]): T[] {
     if (Array.isArray(value)) { return value; }
     if (value) { return [value]; }
     return [];
