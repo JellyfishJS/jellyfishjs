6c71fcf9c3445d426395980315f89e3f03b3db15
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2019-09-30 15:44:33 -0400

    Remove gravity

--- test-games/slippery-racing/src/game-objects/main-object.ts
+++ test-games/slippery-racing/src/game-objects/main-object.ts
@@ -2,7 +2,10 @@ import { game, GameObject } from 'engine';
 import { Car } from './car';
 
 export class MainObject extends GameObject {
+
     public onCreate() {
         (game.createObject as any)(Car, { x: 60, y: 30 });
+        this.physicsWorld.gravity = { x: 0, y: 0, scale: 0 };
     }
+
 }
