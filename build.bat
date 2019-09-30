15b96d7a834fe21021eaba5cb8cebcca6dd2d69c
Quantum <quantum2048@gmail.com>
2019-09-30 13:56:19 -0400

    Add build script for Windows
    Fixes #35

---
+++ build.bat
@@ -0,0 +1,17 @@
+@echo off
+pushd "%~dp0"
+
+pushd engine
+call npm install
+call npm run build
+popd
+
+for /d %%i in (test-games\*) do (
+    pushd "%%~i"
+    if exist package.json (
+        call npm install
+        call npm run build
+    )
+    popd
+)
+popd
