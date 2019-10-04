fa7cc71ea5d859636ac3df72923453a11a72ce53
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2019-10-04 16:29:22 -0400

    Add car interaction

--- test-games/slippery-racing/src/game-objects/car.ts
+++ test-games/slippery-racing/src/game-objects/car.ts
@@ -1,4 +1,5 @@
-import { GameObject } from 'engine';
+import { game, GameObject } from 'engine';
+import * as keycode from 'keycode';
 import { Bodies, Body } from 'matter-js';
 
 export class Car extends GameObject<PIXI.Sprite, Body> {
@@ -21,10 +22,34 @@ export class Car extends GameObject<PIXI.Sprite, Body> {
     }
 
     public draw(pixi: typeof PIXI, sprite: PIXI.DisplayObject) {
-        if (this.physicsBody) {
-            sprite.x = this.physicsBody.position.x;
-            sprite.y = this.physicsBody.position.y;
-            sprite.scale = new pixi.Point(0.1, 0.1);
+        if (!this.physicsBody) { return; }
+
+        sprite.x = this.physicsBody.position.x;
+        sprite.y = this.physicsBody.position.y;
+        sprite.scale = new pixi.Point(0.1, 0.1);
+    }
+
+    public step() {
+        if (!this.physicsBody) { return; }
+
+        const angle = this.physicsBody.angle;
+        const xComponent = Math.cos(angle);
+        const yComponent = Math.sin(angle);
+
+        if (game.keyboard.isDown(keycode('up'))) {
+            Body.applyForce(
+                this.physicsBody,
+                this.physicsBody.position,
+                { x: 0.001 * xComponent, y: 0.001 * yComponent },
+            );
+        }
+
+        if (game.keyboard.isDown(keycode('down'))) {
+            Body.applyForce(
+                this.physicsBody,
+                this.physicsBody.position,
+                { x: -0.001 * xComponent, y: -0.001 * yComponent },
+            );
         }
     }
 
