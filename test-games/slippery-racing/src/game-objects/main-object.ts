59c3e60f438f0ed3a5a87cfa729e23f3329434e5
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2019-10-06 20:18:41 -0400

    Add more decoration

--- test-games/slippery-racing/src/game-objects/main-object.ts
+++ test-games/slippery-racing/src/game-objects/main-object.ts
@@ -6,20 +6,43 @@ import { LooseTire } from './loose-tire';
 
 export class MainObject extends GameObject {
 
+    private generateCircle(center: Vector, size: number) {
+        (game.createObject as any)(Decoration, center, size + 460, 0x888888);
+        (game.createObject as any)(Decoration, center, size + 450, 0xd7d7d7);
+        (game.createObject as any)(Decoration, center, size + 300, 0x44bf4d);
+        (game.createObject as any)(Decoration, center, size + 10, 0x3e7242, true);
+        (game.createObject as any)(Decoration, center, size, 0x45824a);
+    }
+
+    private generateTireCircle(center: Vector, size: number, amount: number) {
+        for (let i = 0; i < 360; i += 360 / amount) {
+            (game.createObject as any)(
+                LooseTire,
+                center.plus(Vector.lengthAndDirection(size, Angle.degrees(i))),
+            );
+        }
+
+        for (let i = 180 / amount; i < 360; i += 360 / amount) {
+            (game.createObject as any)(
+                LooseTire,
+                center.plus(Vector.lengthAndDirection(size + 100, Angle.degrees(i))),
+            );
+        }
+    }
+
     public onCreate() {
 
-        const center = Vector.xy(400, 1000);
-
-        (game.createObject as any)(Decoration, center, 660, 0x888888);
-        (game.createObject as any)(Decoration, center, 650, 0xd7d7d7);
-        (game.createObject as any)(Decoration, center, 500, 0x44bf4d);
-        (game.createObject as any)(Decoration, center, 210, 0x3e7242, true);
-        (game.createObject as any)(Decoration, center, 200, 0x45824a);
+        this.generateCircle(Vector.zero, 200);
+        this.generateCircle(Vector.xy(1200, 1500), 400);
+        this.generateCircle(Vector.xy(-1900, 1500), 800);
+        this.generateCircle(Vector.xy(-1900, -2200), 150);
+        this.generateCircle(Vector.xy(1900, -2700), 150);
+        this.generateCircle(Vector.xy(2900, -2400), 250);
 
         const camera = (game.createObject as any)(Camera) as Camera;
         (game.createObject as any)(
             Car,
-            Vector.xy(60, 1000),
+            Vector.xy(-300, 0),
             {
                 topSpeed: 0.02,
                 acceleration: 0.001,
@@ -30,19 +53,8 @@ export class MainObject extends GameObject {
             camera,
         );
 
-        for (let i = 0; i < 360; i += 15) {
-            (game.createObject as any)(
-                LooseTire,
-                center.plus(Vector.lengthAndDirection(800, Angle.degrees(i))),
-            );
-        }
-
-        for (let i = 7.5; i < 360; i += 15) {
-            (game.createObject as any)(
-                LooseTire,
-                center.plus(Vector.lengthAndDirection(900, Angle.degrees(i))),
-            );
-        }
+        this.generateTireCircle(Vector.zero, 800, 24);
+        this.generateTireCircle(Vector.xy(-2900, -2700), 300, 48);
 
         this.physicsWorld.gravity = { x: 0, y: 0, scale: 0 };
     }
