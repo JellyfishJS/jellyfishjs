126c35e5bf0583ed0cf5bd0f0e3c7dda47141ff3
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2019-10-12 00:05:00 -0400

    Make AnyAmountOf return readonly arrays
    Unfortunately this needed a typecast,
    since typescript doesn't seem to realize
    readonly arrays are also arrays.

--- engine/src/util/as-array.ts
+++ engine/src/util/as-array.ts
@@ -1,14 +1,14 @@
 /**
  * An array of `T`, a single `T`, or `undefined`.
  */
-export type AnyAmountOf<T> = undefined | T | T[];
+export type AnyAmountOf<T> = undefined | T | readonly T[];
 
 /**
  * Takes `undefined`, a `T`, or an array of `T`,
  * and returns an array containing any `T` passed to it.
  */
-export function asArray<T>(value: AnyAmountOf<T>): T[] {
+export function asArray<T>(value: AnyAmountOf<T>): readonly T[] {
     if (Array.isArray(value)) { return value; }
-    if (value) { return [value]; }
+    if (value) { return [value as T]; }
     return [];
 }
