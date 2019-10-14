2d4fc1756c980952d12c98d1a8c65e116b38b864
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2019-10-14 13:03:22 -0400

    Export server from jellyfish

--- engine/src/index.ts
+++ engine/src/index.ts
@@ -1,4 +1,4 @@
 export { Game, game } from './game/game';
 export { GameObject } from './game-object/game-object';
 export { Angle, Vector } from './util/geometry';
-export { User } from './multiplayer';
+export { User, Server } from './multiplayer';
