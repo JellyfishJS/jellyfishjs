de60d456dc955fe13e1cacd5a7fa79f64de92143
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2019-10-14 00:12:40 -0400

    Add socket.io to server class, with basic functionality

--- engine/src/multiplayer/server.ts
+++ engine/src/multiplayer/server.ts
@@ -1,4 +1,6 @@
+import * as SocketIO from 'socket.io';
 import { GameObject } from '../game-object/game-object';
+import { User } from './user';
 
 /**
  * A `Server` is a `GameObject` that,
@@ -7,5 +9,122 @@ import { GameObject } from '../game-object/game-object';
  * and share the state of any child `GameObject`s.
  */
 export class Server extends GameObject {
+
+    /**
+     * The socket.io server this `Server` is using.
+     */
+    private _socketIOServer: SocketIO.Server | undefined;
+
+    /**
+     * A map from user ids to sockets.
+     */
+    private readonly _userToSocket: Map<string, SocketIO.Socket> = new Map();
+
+    /**
+     * A map from user ids to sockets.
+     */
+    private readonly _users: Set<User> = new Set();
+
+    /**
+     * Starts the server on the specified port.
+     *
+     * If no port is specified, uses port `17771`.
+     *
+     * Do not override.
+     */
+    public start(port: number = 17771) {
+        if (this._socketIOServer) { return; }
+
+        this._socketIOServer = SocketIO(port);
+        this._socketIOServer.on('connect', (socket) => {
+            const user = new User();
+            this._userToSocket.set(user.id(), socket);
+            this._users.add(user);
+            this.onUserJoined && this.onUserJoined(user);
+
+            socket.on('message', (type: unknown, contents: unknown) => {
+                this._onMessage(user, type, contents);
+            });
+
+            socket.on('disconnect', (reason: string) => {
+                this._users.delete(user);
+                this._userToSocket.delete(user.id());
+                this.onUserLeft && this.onUserLeft(user, reason);
+            });
+        });
+    }
+
+    /**
+     * Handles a message from the client.
+     *
+     * If it's not formated properly,
+     * logs an error but doesn't throw,
+     * since this might be caused by a client trying to mess with the server.
+     */
+    private _onMessage(user: User, type: unknown, contents: unknown) {
+        if (typeof type !== 'string') {
+            console.error(`Unexpected got message from client with type ${type}, which is not a string.`);
+            return;
+        }
+
+        if (typeof contents !== 'string') {
+            console.error(`Unexpected got message from client with contents ${contents}, which is not a string.`);
+            return;
+        }
+
+        switch (type) {
+            case 'message':
+                this.onMessage && this.onMessage(user, contents);
+                break;
+            default:
+                console.error(`Unexpected got message from client with type ${type}, which is not recognized.`);
+                return;
+        }
+    }
+
+    /**
+     * Sends the specified string message to the specified user.
+     */
+    public sendMessage(user: User, message: string) {
+        const socket = this._userToSocket.get(user.id());
+        if (!socket) { return; }
+
+        socket.send('message', message);
+    }
+
+    /**
+     * Sends the specified string message to all users.
+     */
+    public broadcast(message: string) {
+        const { value: socket } = this._userToSocket.values().next() as { value: SocketIO.Socket | undefined };
+        if (!socket) { return; }
+
+        socket.broadcast.send('message', message);
+    }
+
+    /**
+     * Called when a user joins this server.
+     *
+     * Meant to be overridden.
+     */
+    public onUserJoined?(user: User): void;
+
+    /**
+     * Called when a user leaves this server.
+     *
+     * The reason is a text description of why they left.
+     * See https://socket.io/docs/server-api/#Event-%E2%80%98disconnect%E2%80%99
+     * for possible reasons.
+     *
+     * Meant to be overridden.
+     */
+    public onUserLeft?(user: User, reason: string): void;
+
+    /**
+     * Called when a user sends the server a message.
+     *
+     * Meant to be overridden.
+     */
+    public onMessage?(user: User, message: string): void;
 
 }
