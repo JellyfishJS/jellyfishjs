eebc0122345eb51cbf2d7a71a7f26b6062001c32
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2019-10-13 23:09:53 -0400

    Add an empty Server class

--- engine/src/multiplayer/index.ts
+++ engine/src/multiplayer/index.ts
@@ -1,1 +1,2 @@
 export { User } from './user';
+export { Server } from './server';
---
+++ engine/src/multiplayer/server.ts
@@ -0,0 +1,11 @@
+import { GameObject } from '../game-object/game-object';
+
+/**
+ * A `Server` is a `GameObject` that,
+ * when it exists in a server environment,
+ * allows players to connect,
+ * and share the state of any child `GameObject`s.
+ */
+export class Server extends GameObject {
+
+}
