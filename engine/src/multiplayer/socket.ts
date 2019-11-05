d9c1a7be81f2f26ab4938a3a2e29a91283143c08
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2019-11-05 15:13:05 -0500

    Improve documentation

--- engine/src/multiplayer/socket.ts
+++ engine/src/multiplayer/socket.ts
@@ -1,8 +1,16 @@
 import * as SocketIOForType from 'socket.io';
 import { isServer } from './is-server';
 
+/**
+ * Returns the socket.io dependency if it is available,
+ * otherwise undefined.
+ */
 export function getSocketIO(): typeof SocketIOForType | undefined {
     if (isServer) {
+        // Typescript doesn't generate a require for the first import,
+        // if it is re-required here.
+        // This makes it not crash client-side,
+        // since socket.io cannot be imported in the browser.
         return require('socket.io');
     }
 }
