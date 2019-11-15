bb611a0996602a08146c2906a4fad297dbe1fbe3
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2019-11-15 02:14:06 -0500

    Update terminology and documentation in serializer

--- engine/src/serialization/serializer.ts
+++ engine/src/serialization/serializer.ts
@@ -3,21 +3,25 @@ import { Serialization } from './serialization';
 import { SerializableEntity, SerializedEntity } from './serialization-result';
 
 /**
- * Serializes specified objects.
+ * Serializes and deserializes entities.
  *
- * Multiple instances can be used to use different configuration.
+ * Multiple instances can be used with different configuration.
+ * Instances can be reused if the configuration will rename the same.
  */
 export class Serializer {
 
     /**
-     * Serializes the specified object.
+     * Serializes the specified entity.
      */
-    public serialize(object: SerializableEntity): SerializedEntity {
-        return new Serialization(object).getSerialization();
+    public serialize(entity: SerializableEntity): SerializedEntity {
+        return new Serialization(entity).getSerialization();
     }
 
-    public deserialize(object: SerializedEntity): SerializableEntity {
-        return new Deserialization(object).getDeserialization();
+    /**
+     * Deserializes the specified entity.
+     */
+    public deserialize(entity: SerializedEntity): SerializableEntity {
+        return new Deserialization(entity).getDeserialization();
     }
 
 }
