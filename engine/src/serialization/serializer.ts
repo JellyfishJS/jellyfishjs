b32e928f49755b7764a592accae1df3fd27370bc
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2019-11-17 14:08:06 -0500

    Add class registration to the serializer

--- engine/src/serialization/serializer.ts
+++ engine/src/serialization/serializer.ts
@@ -24,4 +24,33 @@ export class Serializer {
         return new Deserialization(entity, toUpdate).getDeserialization();
     }
 
+    /**
+     * Maps the name of a prototype to a configuration.
+     */
+    private _prototypeNameToConfiguration = new Map<string, PrototypeConfiguration>();
+
+    /**
+     * Maps prototype objects to the names of the prototypes.
+     */
+    private _prototypeToName = new WeakMap<{}, string>();
+
+    /**
+     * Registers a class to be serializable.
+     */
+    public registerClass(Class: new () => unknown) {
+        const { name, prototype } = Class;
+        if (this._prototypeNameToConfiguration.has(name)) {
+            throw new Error(`Serialization registration error: Duplicate name ${name}`);
+        }
+        this._prototypeToName.set(prototype, name);
+
+        const configuration = { prototype };
+
+        this._prototypeNameToConfiguration.set(prototype, configuration);
+    }
+
+}
+
+interface PrototypeConfiguration {
+    prototype: {};
 }
