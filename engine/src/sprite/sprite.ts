a7161f8735b1b133f5740035032507ef70476be9
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2020-07-02 04:48:16 -0400

    Add sprite class

---
+++ engine/src/sprite/sprite.ts
@@ -0,0 +1,20 @@
+import type * as PIXI from 'pixi.js';
+
+export const spriteDrawKey = Symbol('spriteDrawKey');
+export const pixiSpriteKey = Symbol('pixiSpriteKey');
+
+export abstract class Sprite {
+    private [pixiSpriteKey]: PIXI.Sprite | undefined;
+
+    public [spriteDrawKey](pixi: typeof PIXI, container: PIXI.Container) {
+        if (!this[pixiSpriteKey]) {
+            this[pixiSpriteKey] = this.initializeSprite(pixi, container);
+        }
+
+        this.draw(pixi, this[pixiSpriteKey]!, container);
+    }
+
+    public abstract initializeSprite(pixi: typeof PIXI, container: PIXI.Container): PIXI.Sprite;
+
+    public abstract draw(pixi: typeof PIXI, pixiSprite: PIXI.Sprite, container: PIXI.Container): undefined;
+}
