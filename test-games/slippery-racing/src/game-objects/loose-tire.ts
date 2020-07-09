bd579a53212462ac829d7a3302631b82297215e0
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2020-07-09 03:49:56 -0400

    Add ImageSprite to loose tire

--- test-games/slippery-racing/src/game-objects/loose-tire.ts
+++ test-games/slippery-racing/src/game-objects/loose-tire.ts
@@ -1,14 +1,16 @@
-import { Angle, GameObject, Sprite, Vector } from 'engine';
+import { Angle, GameObject, ImageSprite, Sprite, Vector } from 'engine';
 import { Bodies, Body } from 'matter-js';
 
 export class LooseTire extends GameObject<Body> {
 
     private readonly initialPosition: Vector;
     private readonly radius = 20;
+    private readonly sprite: ImageSprite;
 
     public constructor(position: Vector) {
         super();
         this.initialPosition = position;
+        this.sprite = this.createSprite(ImageSprite, '/assets/tire.png');
     }
 
     public setUpPhysicsBody() {
@@ -23,31 +25,10 @@ export class LooseTire extends GameObject<Body> {
                 inertia: 100,
             },
         );
-        this.createSprite(LooseTireSprite, body);
+
+        this.sprite.body = body;
+
         return body;
-    }
-
-}
-
-class LooseTireSprite extends Sprite {
-
-    public physicsBody: Body;
-
-    public constructor(physicsBody: Body) {
-        super();
-        this.physicsBody = physicsBody;
-    }
-
-    public initializeSprite(pixi: typeof PIXI, container: PIXI.Container) {
-        const sprite = pixi.Sprite.from('./assets/tire.png');
-        sprite.anchor.set(0.5);
-        container.addChild(sprite);
-        return sprite;
-    }
-
-    public draw(pixi: typeof PIXI, sprite: PIXI.Sprite) {
-        [sprite.x, sprite.y] = [this.physicsBody.position.x, this.physicsBody.position.y];
-        sprite.angle = Angle.radians(this.physicsBody.angle).degrees();
     }
 
 }
