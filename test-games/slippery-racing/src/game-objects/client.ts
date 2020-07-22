4d45eb0f5b91795a16a2524f957fd49db7d8d7c7
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2020-07-22 14:02:41 -0400

    Re-add objects to slippery racing

--- test-games/slippery-racing/src/game-objects/client.ts
+++ test-games/slippery-racing/src/game-objects/client.ts
@@ -3,6 +3,8 @@ import { Client, game } from 'engine';
 export class SlipperyClient extends Client {
 
     public onCreate() {
+        this.game().getWorld()!.gravity = { x: 0, y: 0, scale: 0 };
+
         this.connect();
     }
 
