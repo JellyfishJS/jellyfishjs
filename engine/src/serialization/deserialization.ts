567f16e6666e8c166234ad29d937d219caf53814
Quantum <quantum2048@gmail.com>
2020-08-15 16:21:45 -0400

    Delete missing symbol keys in deserialization
    Fixes #167

--- engine/src/serialization/deserialization.ts
+++ engine/src/serialization/deserialization.ts
@@ -16,6 +16,7 @@ import {
     SerializedPropertyType,
 } from './serialization-result';
 import type { PrototypeConfiguration, SerializerConfiguration } from './serializer-configuration';
+import symbols = Mocha.reporters.Base.symbols;
 
 /**
  * A class used to deserialize a single entity.
@@ -347,13 +348,31 @@ export class Deserialization {
         properties: { [key: string]: SerializedProperty },
         blacklist?: Set<symbol>,
     ) {
-        Object.keys(properties).forEach((key) => {
-            const symbol = this._getSymbolFromName(key);
+        const newKeys: [string, symbol][] = Object.keys(properties).map((key) => [key, this._getSymbolFromName(key)]);
+        const newKeysSet = new Set(newKeys.map(([key, symbol]) => symbol));
 
+        this._getSymbolsOnObject(item).forEach((symbol) => {
+            if (!newKeysSet.has(symbol)) {
+                delete item[symbol as any];
+            }
+        });
+
+        newKeys.forEach(([key, symbol]) => {
             if (!blacklist || !blacklist.has(symbol)) {
                 item[symbol as any] = this._deserializePropertyValue(properties[key], item[symbol as any]);
             }
         });
+    }
+
+    private _getSymbolsOnObject(
+        item: any,
+    ): Set<symbol> {
+        const symbols: Set<symbol> = new Set();
+        do {
+            Object.getOwnPropertySymbols(item).forEach((symbol) => symbols.add(symbol));
+            item = item.prototype;
+        } while (item);
+        return symbols;
     }
 
     /**
